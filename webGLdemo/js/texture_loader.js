66ee9bec7092b9ae37ba3a4f99b3b9f8c9828f9d
ems236 <ems236@case.edu>
2019-12-11 21:09:50 -0500

    texture fixes

--- webGLdemo/js/texture_loader.js
+++ webGLdemo/js/texture_loader.js
@@ -1,4 +1,4 @@
-function load_texture(gl, image_id)
+function load_texture(gl, scene, image_name)
 {
     const texture = gl.createTexture();
     const level = 0;
@@ -9,10 +9,23 @@ function load_texture(gl, image_id)
     const srcFormat = gl.RGBA;
     const srcType = gl.UNSIGNED_BYTE;
 
-    const image = $("#" + image_id)[0];
+    const temp_texture = new Uint8Array([0, 0, 255, 255]);  // opaque blue
     gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, srcFormat, srcType, image);
-    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texImage2D(gl.TEXTURE_2D, level, internalFormat,
+                width, height, border, srcFormat, srcType,
+                temp_texture);
+
+    const image = new Image();
+    image.crossOrigin = "*";
+    image.onload = function()
+    {
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, srcFormat, srcType, image);
+        gl.generateMipmap(gl.TEXTURE_2D);
+
+        scene.redraw();
+    }
+    image.src = "http://ems236.github.io/RenderingFur/webGLdemo/textures/" + image_name;
 
     return texture;
 }
