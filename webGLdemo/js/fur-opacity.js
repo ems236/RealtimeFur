b0c0edbf2ecbd79d0f57acd50ab08f7cc8537d1f
zhathawa <zach.hathaway48@gmail.com>
2019-12-12 22:48:06 -0500

    Hair map
    New functions to create an opacity map and a sampling function based on the opacity map and a given threshold. Opacities are between 0 and 255.

---
+++ webGLdemo/js/fur-opacity.js
@@ -0,0 +1,52 @@
+function generateFurMap(mapSize, kernelSize) {
+    var uniformNoise = (() => {
+        var noise = new Array(mapSize);
+        for (var i = 0; i < mapSize; i++) {
+            var innerNoiseArray = new Array(mapSize);
+            for (var j = 0; j < mapSize; j++) {
+                innerNoiseArray[j] = Math.random() * 255;
+            }
+            noise[i] = innerNoiseArray;
+        }
+        return noise;
+    })();
+
+    // gaussian filter
+    var filteredNoise = (() => {
+        // get a deep copy
+        var toFilter = new Array(uniformNoise.length);
+
+        // average over kernel
+        var edge = Math.floor(kernelSize / 2);
+
+        for (var px = 0; px < toFilter.length; px++) {
+            toFilter[px] = new Array(uniformNoise.length).fill(0);
+            for (var py = 0; py < toFilter.length; py++) {
+                var count = 0;
+
+                for (var rtl = -edge; rtl <= edge; rtl++) {
+                    if ((rtl + py >= 0) && (rtl + py < kernelSize)) {
+                        for (var ttb = -edge; ttb <= edge; ttb++) {
+                            if ((ttb + px >= 0) && (ttb + px < kernelSize)) {
+                                toFilter[px][py] += uniformNoise[ttb + px][rtl + py];
+                                count++;
+                            }
+                        }
+                    }
+                }
+
+                toFilter[px][py] /= count;
+
+            }
+        }
+        return toFilter;
+    })();
+
+    return filteredNoise;
+}
+
+function sampleFur(threshold, opmap) {
+    return opmap.map(out => {
+        return out.map(item => item > threshold ? item : 0);
+    });
+}
